0d5a9f95a073cec3b5065e416ddf98c215521eda
yafiabiyyu <yafiabiyyu@Abiyyus-MacBook-Pro.local>
2022-03-20 03:57:01 +0700

    increase swap

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ while True:
         deadline = timestamp + 60 * 1
         avax_amount = w3.toWei(random.uniform(0.1,1), 'ether')
         usdc_amount = w3.toWei(random.uniform(1,100), 'ether')
-        for _ in range(5):
+        for _ in range(3):
             if balance > w3.toWei(2,'ether'):
                 amount = int(w3.fromWei(balance, 'ether')) - 1
                 tx = avax_usdc(w3.toWei(amount, 'ether'), deadline)
@@ -90,22 +90,22 @@ while True:
                     print("Waiting for tx to be mined...")
                     print("==================================")
             sleep(2)
-            if balance >= w3.toWei(2, 'ether') and balance > avax_amount:
-                tx = avax_usdc(avax_amount, deadline)
-                print("Swapping {} AVAX".format(w3.fromWei(avax_amount,'ether')))
+        if balance >= w3.toWei(2, 'ether') and balance > avax_amount:
+            tx = avax_usdc(avax_amount, deadline)
+            print("Swapping {} AVAX".format(w3.fromWei(avax_amount,'ether')))
+            print("Tx: {}".format(tx.hex()))
+            print("Waiting for tx to be mined...")
+            print("==================================")
+        else:
+            usdc_balance = usdc.functions.balanceOf(w3.eth.default_account).call()
+            if usdc_balance >= w3.toWei(1, 'ether') and usdc_balance > usdc_amount:
+                tx = usdc_avax(usdc_amount, deadline)
+                print("Swapping {} USDC for {} AVAX".format(w3.fromWei(usdc_amount,'ether'), w3.fromWei(avax_amount,'ether')))
                 print("Tx: {}".format(tx.hex()))
                 print("Waiting for tx to be mined...")
                 print("==================================")
             else:
-                usdc_balance = usdc.functions.balanceOf(w3.eth.default_account).call()
-                if usdc_balance >= w3.toWei(1, 'ether') and usdc_balance > usdc_amount:
-                    tx = usdc_avax(usdc_amount, deadline)
-                    print("Swapping {} USDC for {} AVAX".format(w3.fromWei(usdc_amount,'ether'), w3.fromWei(avax_amount,'ether')))
-                    print("Tx: {}".format(tx.hex()))
-                    print("Waiting for tx to be mined...")
-                    print("==================================")
-                else:
-                    continue
+                continue
         sleep(1)
     except ValueError:
         pass
